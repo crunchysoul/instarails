8286b7327dbcace07ebdf96de4d854588c34f194
crunchysoul <zzzcca12@gmail.com>
2017-09-27 16:23:25 +1000

    Add Geocoder feature

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,14 +1,20 @@
 class Photo < ApplicationRecord
   include ImageUploader::Attachment.new(:image)
 
+  # attr_accessor :raw_address
+
   belongs_to :user
   has_and_belongs_to_many :likers, class_name: 'User', join_table: :likes
   has_many :comments
 
   geocoded_by :location   # can also be an IP address
-  # after_validation :geocode          # auto-fetch coordinates
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode
   after_validation :geocode, if: ->(obj){ obj.location.present? and obj.location_changed? }
-  
+  after_validation :reverse_geocode, unless: ->(obj) { obj.location.present? },
+    if: ->(obj){ obj.latitude.present? and obj.latitude_changed? and obj.longitude.present? and obj.longitude_changed? }
+
+
   # group_photo.liked_by?(jo)
   def liked_by?(user)
     likers.exists?(user.id)
