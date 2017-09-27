dd7bbb2fb552d1b8db9e8523c53414b663094ee0
crunchysoul <zzzcca12@gmail.com>
2017-09-27 11:50:22 +1000

    Add Like Button

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -42,7 +42,12 @@ class PhotosController < ApplicationController
   # PATCH/PUT /photos/1.json
   def update
     respond_to do |format|
-      if @photo.update(photo_params)
+      if is_liking?
+        @photo.toggle_liked_by(current_user)
+        format.html { redirect_to @photo }
+        format.json { render :show, status: :ok, location: @photo }
+      elsif
+        @photo.update(photo_params)
         format.html { redirect_to @photo, notice: 'Photo was successfully updated.' }
         format.json { render :show, status: :ok, location: @photo }
       else
@@ -63,13 +68,18 @@ class PhotosController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_photo
-      @photo = Photo.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_photo
+    @photo = Photo.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def photo_params
-      params.require(:photo).permit(:image, :description)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def photo_params
+    params.require(:photo).permit(:image, :description)
+  end
+
+  def is_liking?
+    # is there liked view
+    params.require(:photo)[:liked].present?
+  end
 end
